59d0c0a9eaf1b4965aa7096e5d4c759be590b833
vanshika-1136 <vanshikadhariya9@gmail.com>
2026-04-04 12:58:58 +0530

    recursion and staticAndDynamicMemory folders

---
+++ recursion/sayDigit.cpp
@@ -0,0 +1,22 @@
+//123
+//one two three
+#include<iostream>
+using namespace std;
+void sayDigit(int n,string arr[]){
+    if(n==0){
+        return ;
+    }
+    int digit=n%10;
+    n=n/10;
+    sayDigit(n,arr); 
+    cout<<arr[digit]<<" ";    
+}
+int main(){
+    string arr[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    int n;
+    cin>>n;
+    cout<<endl;
+    sayDigit(n,arr);
+    return 0;
+
+}
